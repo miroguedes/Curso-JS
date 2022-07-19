e19fd419e3b4a2c2f7a851d8329cacf4c5f3dac8
Miro Guedes <consultormiro@live.com>
2022-07-19 13:31:37 -0300

    a

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 12
+var vel = 59
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
 if (vel > 60) { //condição simples
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
-console.log(`Dirija sempre usando cinto de seguraça`)
+console.log(`Dirija sempre usando o cinto de seguraça`)
